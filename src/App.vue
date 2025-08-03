9492ec1c2355199c8326675ccbb9514a75dc337f
AsmaaAK <asoomak97@gmail.com>
2025-08-03 10:27:41 -0700

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
-
+  <div class="min-h-screen flex flex-col">
+    <Navbar />
+    <div class="flex flex-1">
+      <Sidebar />
+      <router-view />
+    </div>
+    <Footer />
+  </div>
 </template>
 
-<script>
-
-
+<script setup>
+import Navbar from './components/Navbar.vue'
+import Sidebar from './components/Sidebar.vue'
+import Footer from './components/Footer.vue'
 </script>
